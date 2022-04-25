3957a0024b83de5c0761e3fbd11073e75ee6cd02
AkkoLeTaco <Liciaga.Angelo@live.jobcorps.org>
2022-04-25 18:13:11 +0000

    complete v1.2

--- src/js/component/Details.jsx
+++ src/js/component/Details.jsx
@@ -10,17 +10,43 @@ export const Details = () => {
 	return (
 		<>
 			{store.CharacterDescription.result ? 
-				<div className="card mb-3" style={{ width: "540px" }}>
+				<div className="card mb-3" style={{ width: "1000px" }}>
 					<div className="row g-0">
-						<div className="col-md-4">
-							<img src={store.img + store.CharacterDescription.result.uid + ".jpg"} className="img-fluid rounded-start" alt="..." />
-						</div>
+						<div className="col">
+									<img src={store.img + store.CharacterDescription.result.uid + ".jpg"} className="img-fluid rounded-start" alt="..." />
+								</div>
 						<div className="col-md-8">
 							<div className="card-body">
-								<h5 className="card-title">{store.CharacterDescription.result.properties.name}</h5>
-								<p className="card-text">{store.CharacterDescription.result.description}</p>
-								<p className="card-text text-danger"><small>Height: {store.CharacterDescription.result.properties.height}<br></br>Mass: {store.CharacterDescription.result.properties.mass}<br></br>Hair Color: {store.CharacterDescription.result.properties.hair_color}<br></br>Birth Year: {store.CharacterDescription.result.properties.birth_year}<br></br>Skin Color: {store.CharacterDescription.result.properties.skin_color}</small></p>
+								
+								<div className="col justify-content-right">
+									<h5 className="card-title">{store.CharacterDescription.result.properties.name}</h5>
+									<p className="card-text">{store.CharacterDescription.result.description}</p>
+								</div>
+								
 							</div>
+						</div>
+					</div>
+					<hr></hr>
+					<div className="d-flex d-flex justify-content-evenly text-danger">
+						<div>
+							<h4>Height:</h4>
+							<p>{store.CharacterDescription.result.properties.height}</p>
+						</div>
+						<div>
+							<h4>Mass:</h4>
+							<p>{store.CharacterDescription.result.properties.mass}</p>
+						</div>
+						<div>
+							<h4>Birth Year:</h4>
+							<p>{store.CharacterDescription.result.properties.birth_year}</p>
+						</div>
+						<div>
+							<h4>Hair Color:</h4>
+							<p>{store.CharacterDescription.result.properties.hair_color}</p>
+						</div>
+						<div>
+							<h4>Skin Color:</h4>
+							<p>{store.CharacterDescription.result.properties.skin_color}</p>
 						</div>
 					</div>
 				</div>
